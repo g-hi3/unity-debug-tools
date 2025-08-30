88126e9d8b7aaf5d90f95367f01210fdb1994039
g-hi3 <marc.forrer@bluewin.ch>
2025-08-30 14:52:35 +0200

    improved Segmented Line sample

---
+++ Samples~/Segmented Line/SegmentedLine.cs
@@ -0,0 +1,53 @@
+﻿using G_hi3.Debug;
+using UnityEngine;
+
+public sealed class SegmentedLine : MonoBehaviour
+{
+    [SerializeField]
+    private Transform start;
+
+    [SerializeField]
+    private Transform end;
+
+    [SerializeField]
+    private Color segmentColor;
+
+    [SerializeField]
+    private float segmentLength;
+
+    [SerializeField]
+    private float segmentSpacing;
+
+    [SerializeField]
+    private float timeScale;
+
+    [SerializeField]
+    private bool depthTest;
+
+    private void Update()
+    {
+        if (start?.position is { } startPosition
+            && end?.position is { } endPosition)
+        {
+            DebugTools.DrawSegmentedLine(
+                startPosition,
+                endPosition,
+                segmentLength,
+                segmentSpacing,
+                segmentColor,
+                timeScale,
+                depthTest);
+        }
+    }
+
+    private void Reset()
+    {
+        start = null;
+        end = null;
+        segmentColor = Color.green;
+        segmentLength = 0.2f;
+        segmentSpacing = 0.1f;
+        timeScale = 1f;
+        depthTest = true;
+    }
+}
